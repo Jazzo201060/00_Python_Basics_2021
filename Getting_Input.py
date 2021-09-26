dc1e6a3b0777d2a0c735886b77350824852c7262
Jazzo201060 <201060@trinityschools.nz>
2021-09-27 12:47:56 +1300

    Learning Python

---
+++ Getting_Input.py
@@ -0,0 +1,5 @@
+username = input("What is your name? ")
+favint = int(input('Favorite number: '))
+chint = favint**2
+print('Hello, ', username, '. Your favorite number squared is: ', chint)
+
---
+++ Hello_World.py
@@ -0,0 +1,3 @@
+msg = input('message:')
+for i in range(0,5):
+    print(msg)
